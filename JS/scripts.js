1260adaac4fb3f65e27bfb4becbbcf9e4dbe9d72
Maria Clara de Castro <mariaclarabfc.eng@gmail.com>
2022-04-20 14:05:19 -0300

    Inicio JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,41 @@
+const APPI_URL = "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes"
+
+const JogarNovoQuizz = document.querySelector(".pagina_quizz")
+const CriarNovoQuizz = document.querySelector(".criar_novo_quizz")
+const PaginaInicial = document.querySelector(".todos-os-quizzes")
+const CarregandoQuizzes = document.querySelector(".carregar-pagina")
+
+let quizzesDisponíveis  = {
+    id:"",
+    title: "",
+    image:"",
+    questions:[],
+    levels:[],
+}
+
 function recarregar(){
     window.location.reload();
+
 }
+
+function carregandoQuizzes(){
+    CarregandoQuizzes.classList.remove("escondido")
+    
+    if (CarregandoQuizzes.classList.contains("quizzCarregado")){
+        CarregandoQuizzes.classList.add("escondido")
+        CarregandoQuizzes.classList.remove("quizzCarregado")
+    }
+    else{
+        CarregandoQuizzes.classList.add("quizzCarregando")
+    }
+
+}
+
+function quizzesCarregados(){
+    CarregandoQuizzes.classList.add("quizzCarregado")
+    if(CarregandoQuizzes.classList.contains("quizzCarregando")){
+        CarregandoQuizzes.classList.remove("escondido")
+        CarregandoQuizzes.classList.remove("quizzCarregando")
+    }
+}
+
