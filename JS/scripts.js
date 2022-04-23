b87a75825c1b0cf52efbd0722817fb9806e0e3ff
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-04-23 04:32:57 -0300

    Tela de inserção de dados inicias implementada junto com as verificações necessárias - Elementos CSS e HTML adequados

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,14 +1,15 @@
+//VARIÁVEIS GLOBAIS
 const APPI_URL = "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes";
-
-const JogarNovoQuizz = document.querySelector(".pagina_quizz");
-const CriarNovoQuizz = document.querySelector(".criar_novo_quizz");
 const paginaQuizz = document.querySelector(".tela-quiz");
-
-const CarregandoQuizzes = document.querySelector(".carregar-pagina");
 let dadosQuizz;
-
-let quizzesDisponiveis  = [];
-
+let quizzesDisponiveis=[];
+const InformacaoDoQuiz = {
+    questionsAnswered: 0,
+    rightAnswers: 0
+};
+//const JogarNovoQuizz = document.querySelector(".pagina_quizz");
+//const CriarNovoQuizz = document.querySelector(".criar_novo_quizz");
+//const CarregandoQuizzes = document.querySelector(".carregar-pagina");
 /*const quizzesValidos = {
     id:"",
     title: "", 
@@ -16,46 +17,36 @@ let quizzesDisponiveis  = [];
     questions: [],
     levels: []
 }*/
-
-const InformacaoDoQuiz = {
-    questionsAnswered: 0,
-    rightAnswers: 0
-};
-
+//CHAMANDO FUNÇÃO
 carregarTodosQuizzes();
 
 function recarregarPagina(){
     window.scrollTo(1,0);
     window.location.reload();
 }
-
-function carregandoQuizzes(){
-    CarregandoQuizzes.classList.remove("escondido");
+// function carregandoQuizzes(){
+//     CarregandoQuizzes.classList.remove("escondido");
     
-    if (CarregandoQuizzes.classList.contains("quizzCarregado")){
-        CarregandoQuizzes.classList.add("escondido");
-        CarregandoQuizzes.classList.remove("quizzCarregado");
-    }
-    else{
-        CarregandoQuizzes.classList.add("quizzCarregando");
-    }
-
-}
-
-function quizzesCarregados(){
-    CarregandoQuizzes.classList.add("quizzCarregado");
-    if(CarregandoQuizzes.classList.contains("quizzCarregando")){
-        CarregandoQuizzes.classList.remove("escondido");
-        CarregandoQuizzes.classList.remove("quizzCarregando");
-    }
-}
-
+//     if (CarregandoQuizzes.classList.contains("quizzCarregado")){
+//         CarregandoQuizzes.classList.add("escondido");
+//         CarregandoQuizzes.classList.remove("quizzCarregado");
+//     }
+//     else{
+//         CarregandoQuizzes.classList.add("quizzCarregando");
+//     }
+// }
+// function quizzesCarregados(){
+//     CarregandoQuizzes.classList.add("quizzCarregado");
+//     if(CarregandoQuizzes.classList.contains("quizzCarregando")){
+//         CarregandoQuizzes.classList.remove("escondido");
+//         CarregandoQuizzes.classList.remove("quizzCarregando");
+//     }
+// }
 function carregarTodosQuizzes(){
     const promise=axios.get(APPI_URL);
     promise.then(renderizarQuizzes);
     promise.catch(tratarErroCarregarQuizzes);
 }
-
 function renderizarQuizzes(resposta){
     quizzesDisponiveis=[resposta.data];
     qntQuizzes=quizzesDisponiveis[0].length;
@@ -75,7 +66,6 @@ function tratarErroCarregarQuizzes(erro){
     alert(`Erro ao carregar os quizzes existentes: Erro número ${erro.response.status}`);
     recarregarPagina();
 }
-
 function abrirPaginaQuizz(elemento){
     const ID_DO_QUIZZ=elemento.id;
     const promise=axios.get(`${APPI_URL}/${ID_DO_QUIZZ}`);
@@ -89,7 +79,6 @@ function renderizarQuizz(resposta){
     dadosQuizz = resposta.data;
     exibirQuizz(resposta.data);
 }
-
 function tratarErroAbrirQuizz(erro){
     alert(`Erro ao abri o quizz selecionado: Erro número ${erro.response.status}`);
     recarregarPagina();
@@ -97,7 +86,6 @@ function tratarErroAbrirQuizz(erro){
 function embaralha() {
     return Math.random() -0.5;
 }
-
 function exibirQuizz (quizz){
     title= quizz.title;
     bannersrc = quizz.image;
@@ -134,7 +122,6 @@ function exibirQuizz (quizz){
         document.getElementById(`pergunta ${1}`).parentNode.scrollIntoView( {block: "center"});
 },2000)
 }
-
 function escolherResposta(element){
     const respostas = element.parentNode.querySelectorAll("li");
     const qntRespostas=respostas.length;
@@ -164,7 +151,6 @@ function escolherResposta(element){
 function pegarNumeroIdUl(str){
     return  Number(str.replace("pergunta ",""));
 }
-
 function finalizarQuizz(){
     const elementosRespostas=document.querySelectorAll("li>span");
     InformacaoDoQuiz.questionsAnswered=Array.from(elementosRespostas).filter(elemento=>elemento.style.color==="rgb(0, 156, 34)").length;
@@ -173,7 +159,6 @@ function finalizarQuizz(){
         mostrarResultado();
     }
 }
-
 function zerarQuizz (){
     paginaQuizz.innerHTML = "";
     exibirQuizz(dadosQuizz);
@@ -181,7 +166,6 @@ function zerarQuizz (){
     InformacaoDoQuiz.questionsAnswered =0;
     InformacaoDoQuiz.rightAnswers=0;
 }
-
 function mostrarResultado(){
      const score = Math.round((InformacaoDoQuiz.rightAnswers/InformacaoDoQuiz.questionsAnswered)*100);
      console.log(score);
@@ -207,24 +191,61 @@ function mostrarResultado(){
         document.querySelector(".quiz-results").scrollIntoView();
     },2000);
 }
-
 function criarQuizz(){
     document.querySelector(".tela-inicial").classList.add("escondido");
-    document.querySelector(".Tela-de-criar-quizz").classList.remove("escondido");
+    document.querySelector(".tela-de-criar-quizz").classList.remove("escondido");
 }
+function verificarDadosIniciais(){
+    const titulo=document.getElementById("titulo").value;
+    const urlImagem=document.getElementById("url").value;
+    const numDePerguntas=document.getElementById("perguntas").value;
+    const numeroDeNiveis=document.getElementById("niveisquizz").value;
+    document.getElementById("titulo").value="";
+    document.getElementById("url").value="";
+    document.getElementById("perguntas").value="";
+    document.getElementById("niveisquizz").value="";
+    //Título do quizz: deve ter no mínimo 20 e no máximo 65 caracteres
+    if(isValidTitle(titulo)){
+        return alert("Título inserido é inválido! Digite um titulo com no mínimo 20 caracteres e no máximo 65 caracteres");
+    }
+    if(!isValidURL(urlImagem)){
+        return alert("URL inserida é inválida! Digite uma URL válida!");
+    }
+    if(isNaN(Number(numDePerguntas)) || Number(numDePerguntas)<3){
+        return alert("Número de perguntas inválido! Digite um número maior ou igual a 3");
+    }
+    if(isNaN(Number(numeroDeNiveis)) || Number(numeroDeNiveis)<2){
+        return alert("Número de níveis inválido! Digite um número maior ou igual a 2");
+    }
+    document.querySelector(".tela-de-criar-quizz").classList.add("escondido");
+    document.querySelector(".tela-de-gerar-perguntas").classList.remove("escondido");
+    //IDEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA A CONTINUAR
+    //ADICIONAR OS ELEMENTOS DO HTML DAS PERGUNTAS A SEREM INSERIDAS AQUI
+    //DEPOIS COLOCAR O ONCLICK NO BOTÃO DE PROSSEGUIR E USAR PROVAVELMENTE OS QUATRO ELEMENTOS DEFINIDOS COMO GLOBAIS; 
+}
+function isValidTitle(string){
+    return(typeof(string)!=="string" || string.length<20 || string.length>65);
+}
+function isValidURL(string) {
+    let resposta = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (resposta !== null)
+};
 
-function teste(){
-    let oForm; 
-    let name;
-    oForm = document.forms[0];  
-    
-    name = oForm.elements[0].value;
-    console.log(name);
-}
+// function validURL(str) {
+//     let pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+//       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+//       '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+//       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+//       '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+//       '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+//       console.log(!!pattern.test(str));
+//     return !!pattern.test(str);
+// }
 /*function inicioDaCriacao(quizzBasico){
     let questionValue = "";
 
-    let numeroDeNiveis = "";
+    let numeroDeNiveis=document.getElementById("niveisquizz");
+     = "";
 
     if(!quizzBasico){
         quizzesValidos = {
@@ -237,7 +258,8 @@ function teste(){
     }else {
         quizzesValidos = quizzBasico;
         numeroDeQuestoes = quizzBasico.questions.length;
-        numeroDeNiveis = quizzBasico.levels.length;
+        numeroDeNiveis=document.getElementById("niveisquizz");
+         = quizzBasico.levels.length;
     }
     const telaCriarQuiz = document.querySelector(".quizz-lista");
     telaCriarQuiz.classList.add("escondido");
@@ -260,7 +282,7 @@ function criarPergunta(){
     <button class = "criar-questoes" onclinck = "criarLevels()"> Prosseguir para criar níveis </button>;
 }
 function escreverPerguntas(){
-
 }
-
 */
+
+  
